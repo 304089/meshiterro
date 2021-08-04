9b386ba274149a85911c4a032981ba9db22725b7
304089 <9457imai2@gmail.com>
2021-08-04 08:06:08 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '1f3d6a59483bc2132c5f2a613ec2756e61d8c949b261f01809bb2ea5aba4c1c4326b350498705277df4f7e57d259baceb9d055fb1e4907aaf5cef6d77e0ec74a'
